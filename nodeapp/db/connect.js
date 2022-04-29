76a32432536f3b42bfdc82420df7f6d80fe2fd7b
sbed13@byui.edu <sbed13@byui.edu>
2022-04-29 16:16:28 -0500

    wk 2

---
+++ nodeapp/db/connect.js
@@ -0,0 +1,35 @@
+const {MongoClient} = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
+
+let _db;
+uri = 'mongodb+srv://SierraP:zecc4YKwbelRUwEr@cse341.19dk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect(uri)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
+  
+};
+
+module.exports = {
+  initDb,
+  getDb,
+};
+
+//mongodb+srv://SierraP:zecc4YKwbelRUwEr@cse341.19dk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
