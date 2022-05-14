828ac5dbe4750d74edd3eaf01364626d22d278c0
sbed13@byui.edu <sbed13@byui.edu>
2022-05-13 19:06:29 -0500

    4

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,11 +1,13 @@
+
 const express = require('express')
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const cors = require('cors');
  
 const port = process.env.PORT || 3000;
 const app = express();
  
- 
+  app.use(cors())
   app.use(bodyParser.json())
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,4 +24,19 @@ mongodb.initDb((err, mongodb) => {
   }
 });
  
- 
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Wk 4 API',
+    description: 'Contacts API'
+  },
+  host: 'localhost:3000',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['nodeapp/routes/index.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
