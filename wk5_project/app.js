f66ae607ed7e4b353f3cc7f05586ac6af12752d5
sbed13@byui.edu <sbed13@byui.edu>
2022-05-24 13:02:39 -0500

    graph ql

--- wk5_project/app.js
+++ wk5_project/app.js
@@ -4,20 +4,41 @@ const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 3000;
 const app = express();
- 
-  
+const { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
  
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   });
   app.use('/', require('./routes'));
- 
+
+
+
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
   } else {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
+  
   }
 });
+app.get('/', (req, res) => res.send("This is my Todo-List"))
+
