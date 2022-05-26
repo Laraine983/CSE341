f9d0f3cbd7c7992b3fbc10266d4b5d8bac1ea6ba
sbed13@byui.edu <sbed13@byui.edu>
2022-05-26 18:00:04 -0500

    switch to swagger

--- wk5_project/app.js
+++ wk5_project/app.js
@@ -1,44 +1,42 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-
-const port = process.env.PORT || 3000;
-const app = express();
-const { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-var root = { hello: () => 'Hello world!' };
-
-
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+const cors = require('cors');
 
  
+const port = process.env.PORT || 3000;
+const app = express();
+ 
+  app.use(cors())
+  app.use(bodyParser.json())
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  });
+  })
   app.use('/', require('./routes'));
-
-
-
+ 
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
   } else {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
-  
   }
 });
-app.get('/', (req, res) => res.send("This is my Todo-List"))
+ 
+const swaggerAutogen = require('swagger-autogen')();
 
+const doc = {
+  info: {
+    title: 'Wk 5 API',
+    description: 'Cats API'
+  },
+  host: 'localhost:3000',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['/routes/index.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
