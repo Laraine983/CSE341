e6771cf789c3e60d113540635af5b4d44727511a
sbed13@byui.edu <sbed13@byui.edu>
2022-06-16 22:06:44 -0600

    auth

--- wk5_project/app.js
+++ wk5_project/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const cors = require('cors');
 const { signupValidation, loginValidation } = require('./validation.js');
-const { auth } = require('express-openid-connect');
+
 
  
 const port = process.env.PORT || 3000;
@@ -19,24 +20,9 @@ const app = express();
   app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(
-  auth({
-   // issuerBaseURL: 'https://YOUR_DOMAIN',
-   // baseURL: 'https://YOUR_APPLICATION_ROOT_URL',
-   // clientID: 'YOUR_CLIENT_ID',
-   // secret: 'LONG_RANDOM_STRING',
-   // idpLogout: true,
-  })
-);
-
-app.post('/register', signupValidation, (req, res, next) => {
-  // your registration code
-});
 
 
-app.post('/login', loginValidation, (req, res, next) => {
-  // your login code
-});
+
 
 // Handling Errors
 app.use((err, req, res, next) => {
@@ -73,3 +59,63 @@ const endpointsFiles = ['routes/index.js'];
 
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+app.get('/', function(req, res) {
+  res.render('pages/auth');
+});
+
+// const port = process.env.PORT || 3000;
+// app.listen(port , () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  Google AUTH  */
+ 
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '864164138381-flsdvm2amnl355n8t0e2n1tdchusikbo.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
